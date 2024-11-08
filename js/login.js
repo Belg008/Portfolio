313757b67e64a3f9ae542931b0149432845eb345
Belg008 <sindbelg@gmail.com>
2024-11-08 11:55:23 +0100

    Add files via upload

---
+++ js/login.js
@@ -0,0 +1,9 @@
+function register() {
+    console.log("Register function called");
+    // Resten av koden
+}
+
+function login() {
+    console.log("Login function called");
+    // Resten av koden
+}
